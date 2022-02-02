3581c602cd87c6e4b4bd2a1c16b0fa6451c8aa57
sheey <sheey11@qq.com>
2022-02-02 15:17:18 +0800

    remove notNull constraints in order's coupon field

--- order.go
+++ order.go
@@ -37,8 +37,8 @@ type Order struct {
 	Type            OrderType `json:"type"`
 	GoodID          uint      `json:"-"`
 	Good            Good      `json:"good"`
-	CouponID        uint      `json:"-"`
-	Coupon          Coupon    `json:"-"`
+	CouponID        *uint     `json:"-"`
+	Coupon          *Coupon   `json:"-"`
 	Affiliate       User      `json:"-"`
 	AffiliateID     uint      `json:"-"`
 
